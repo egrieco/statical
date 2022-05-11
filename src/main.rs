d4ca82ba721bb1d5cc865da011a8f19dd1477abd
Elio Grieco <grieco@egx.com>
2022-05-11 14:44:42 -0700

    Read with ical

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
 #![allow(unused_imports)]
 
+extern crate ical;
+
 use clap::{Args, Parser};
 use color_eyre::eyre::{self, WrapErr};
+use ical::IcalParser;
+use std::{fs::File, io::BufReader, path::PathBuf};
 
 use statical::*;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
 struct Opt {
-    /// Tracing filter.
-    ///
-    /// Can be any of "error", "warn", "info", "debug", or
-    /// "trace". Supports more granular filtering, as well; see documentation for
-    /// [`tracing_subscriber::EnvFilter`][EnvFilter].
-    ///
-    /// [EnvFilter]: https://docs.rs/tracing-subscriber/latest/tracing_subscriber/struct.EnvFilter.html
-    #[clap(long, default_value = "info")]
-    tracing_filter: String,
+    /// The calendar file to read
+    #[clap(short, long)]
+    file: PathBuf,
 }
 
 fn main() -> eyre::Result<()> {
@@ -24,6 +22,15 @@ fn main() -> eyre::Result<()> {
     color_eyre::install()?;
 
     println!("Arguments: {:#?}", args);
+    println!("Provided path is: {:?}", args.file);
+    if args.file.exists() {
+        println!("  file exists");
+        let buf = BufReader::new(File::open(args.file)?);
+        let reader = IcalParser::new(buf);
+        for entry in reader {
+            println!("{:#?}", entry);
+        }
+    }
 
     Ok(())
 }
