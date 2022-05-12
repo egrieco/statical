a2baad66f8aff872d097707f33944188613fd651
Elio Grieco <grieco@egx.com>
2022-05-12 09:48:21 -0700

    Allow multiple calendar args

--- src/main.rs
+++ src/main.rs
@@ -14,34 +14,38 @@ use statical::*;
 struct Opt {
     /// The calendar file to read
     #[clap(short, long)]
-    file: Option<PathBuf>,
+    file: Option<Vec<PathBuf>>,
 
     /// The calendar url to read
     #[clap(short, long)]
-    url: Option<String>,
+    url: Option<Vec<String>>,
 }
 
 fn main() -> eyre::Result<()> {
     let args = Opt::parse();
     color_eyre::install()?;
 
+    println!("  Arguments: {:#?}", args);
     let mut events = Vec::new();
 
-    println!("  Arguments: {:#?}", args);
-    if let Some(file) = args.file {
-        println!("  Provided path is: {:?}", file);
-        if file.exists() {
-            println!("    File exists");
-            let buf = BufReader::new(File::open(file)?);
-            events.append(&mut parse_calendar(buf)?);
+    if let Some(files) = args.file {
+        for file in files {
+            println!("  Provided path is: {:?}", file);
+            if file.exists() {
+                println!("    File exists");
+                let buf = BufReader::new(File::open(file)?);
+                events.append(&mut parse_calendar(buf)?);
+            }
         }
     }
 
-    if let Some(url) = args.url {
-        println!("  Provided url is: {:?}", url);
-        let ics_string = ureq::get(&url).call()?.into_string()?;
-        println!("    URL exists");
-        events.append(&mut parse_calendar(ics_string.as_bytes())?);
+    if let Some(urls) = args.url {
+        for url in urls {
+            println!("  Provided url is: {:?}", url);
+            let ics_string = ureq::get(&url).call()?.into_string()?;
+            println!("    URL exists");
+            events.append(&mut parse_calendar(ics_string.as_bytes())?);
+        }
     }
 
     for event in events {
