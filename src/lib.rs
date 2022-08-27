584d6c024961c1edc45938509e656c62cbeb91e3
Elio Grieco <grieco@egx.com>
2022-08-27 16:33:35 -0700

    Add crate level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! # Statical
+//!
+//! `statical` is a calendar aggregator and generator that aims to make maintaining calendars on static websites easier and cheaper.
+//!
+//! It reads a collection of `*.ics` files or calendar feeds and creates a collection of `html` files containing all of the events found in the source files and feeds.
+
 pub mod config;
 pub mod model;
 pub mod options;
